79904f06e710d59da50bcf745fa2dacdfb9d54dd
wongsw <wong.songwei@gmail.com>
2017-04-20 13:50:35 -0500

    HW7 completed

--- HW7.X/hw7_main.c
+++ HW7.X/hw7_main.c
@@ -12,6 +12,8 @@
 #define OUT_TEMP_L_ADDR 0b00100000 // OUT_TEMP_L register address
 #define BG 0xF800 //Background color = RED
 #define TEXTCOLOR 0xFFFF //Text color = WHITE
+#define BARCOLOR 0xFFFF //Bar color = WHITE
+#define MAXBARLEN 50 //Maximum length for drawing bars
 
 // DEVCFG0
 #pragma config DEBUG = OFF // no debugging
@@ -72,22 +74,21 @@ void setExpander(char pin, char level){
     i2c_master_send(manipulate_bits); // config pin to level
     i2c_master_stop(); //make the stop bit
 }
-*/
+ */
 
 void display_char(unsigned char x, unsigned char y, unsigned short color1, char c) {
     int i = 0, j = 0;
 
-    for (i=0; i<5; i++) { // i is the index of columns
-        unsigned short k = ASCII[c-0x20][i]; // k is a byte in the ASCII table
-        for (j=0; j<8; j++) { // j is the index of rows
-            if(x > 128 || y > 128){ // error check that x and y are not out of bounds
+    for (i = 0; i < 5; i++) { // i is the index of columns
+        unsigned short k = ASCII[c - 0x20][i]; // k is a byte in the ASCII table
+        for (j = 0; j < 8; j++) { // j is the index of rows
+            if (x > 128 || y > 128) { // error check that x and y are not out of bounds
                 break;
             }
-            if(k >> j & 1) {
-                LCD_drawPixel(x+i, y+j, color1);
-            }
-            else {   
-                LCD_drawPixel(x+i, y+j, BG);
+            if (k >> j & 1) {
+                LCD_drawPixel(x + i, y + j, color1);
+            } else {
+                LCD_drawPixel(x + i, y + j, BG);
             }
         }
     }
@@ -95,19 +96,86 @@ void display_char(unsigned char x, unsigned char y, unsigned short color1, char 
 
 void display_string(char* msg1, unsigned char x, unsigned char y) {
     int i = 0;
-    while(msg1[i]!=0){
-        display_char(x+(i*6), y, TEXTCOLOR, msg1[i]); // Displace the spacing between letters
+    while (msg1[i] != 0) {
+        display_char(x + (i * 6), y, TEXTCOLOR, msg1[i]); // Displace the spacing between letters
         i++;
     }
 }
 
+// Draw bar in horizontal direction
+void draw_bar_X(unsigned char x, unsigned char y, unsigned char w, unsigned short color1, unsigned short color2, signed short len, unsigned char maxLength) {
+    int i = 0, j = 0;
+    if (len > 0 | len == 0) { // for positive values draw bar to right of start point (x,y)
+        for (j = 0; j < len; j++) { //filled bar progress
+            for (i = 0; i < w; i++) {
+                LCD_drawPixel(x, y + i, color1); //draws the width of bar 
+            }
+            x++;
+        }
+        for (j = len; j < maxLength; j++) { //unfilled bar progress
+            for (i = 0; i < w; i++) {
+                LCD_drawPixel(x, y + i, color2); //draws the width of bar with alt color
+            }
+            x++;
+        }
+    } 
+    else { //for negative values draw bar to left of start point (x,y))
+        for (j = len; j < 0; j++) { //filled bar progress
+            for (i = 0; i < w; i++) {
+                LCD_drawPixel(x, y + i, color1); //draws the width of bar 
+            }
+            x--;
+        }
+        for (j = -maxLength; j < len; j++) { //unfilled bar progress
+            for (i = 0; i < w; i++) {
+                LCD_drawPixel(x, y + i, color2); //draws the width of bar with alt color
+            }
+            x--;
+        }
+    }
+}
+
+// Draw bar in vertical direction
+void draw_bar_Y(unsigned char x, unsigned char y, unsigned char w, unsigned short color1, unsigned short color2, signed short len, unsigned char maxLength) {
+    int i = 0, j = 0;
+    if (len > 0 | len == 0) { // for positive values draw bar to right of start point (x,y)
+        for (j = 0; j < len; j++) { //filled bar progress
+            for (i = 0; i < w; i++) {
+                LCD_drawPixel(x + i, y, color1); //draws the width of bar 
+            }
+            y++;
+        }
+        for (j = len; j < maxLength; j++) { //unfilled bar progress
+            for (i = 0; i < w; i++) {
+                LCD_drawPixel(x + i, y, color2); //draws the width of bar with alt color
+            }
+            y++;
+        }
+    } 
+    else { //for negative values draw bar to left of start point (x,y))
+        for (j = len; j < 0; j++) { //filled bar progress
+            for (i = 0; i < w; i++) {
+                LCD_drawPixel(x + i, y, color1); //draws the width of bar 
+            }
+            y--;
+        }
+        for (j = -maxLength; j < len; j++) { //unfilled bar progress
+            for (i = 0; i < w; i++) {
+                LCD_drawPixel(x + i, y, color2); //draws the width of bar with alt color
+            }
+            y--;
+        }
+    }
+}
+
 //function to check value of WHO_AM_I register, to ensure connections are correct
-char getWHOAMI(void){
+
+char getWHOAMI(void) {
     i2c_master_start(); //ST: start bit
-    i2c_master_send(SLAVE_ADDR<<1|0); //SAD+W: slave address left shifted 1 and add a '0' bit for writing
+    i2c_master_send(SLAVE_ADDR << 1 | 0); //SAD+W: slave address left shifted 1 and add a '0' bit for writing
     i2c_master_send(WHO_AM_I_ADDR); //SUB: send an 8-bit sub-address of the WHO_AM_I register used
     i2c_master_restart(); // SR: make restart bit
-    i2c_master_send(SLAVE_ADDR<<1|1); // SAD+R: slave address left shifted 1 and add a '1' bit for reading
+    i2c_master_send(SLAVE_ADDR << 1 | 1); // SAD+R: slave address left shifted 1 and add a '1' bit for reading
     unsigned char r = i2c_master_recv(); //save value returned
     i2c_master_ack(1); // NMAK
     i2c_master_stop(); //SP: stop bit
@@ -115,24 +183,25 @@ char getWHOAMI(void){
 }
 
 //initialize IMU
-void IMU_init(){
+
+void IMU_init() {
     // Turn on accelerometer (Register CTRL1_XL)
     i2c_master_start(); //ST: start bit
-    i2c_master_send(SLAVE_ADDR<<1|0); //SAD+W: slave address left shifted 1 and add a '0' bit for writing
+    i2c_master_send(SLAVE_ADDR << 1 | 0); //SAD+W: slave address left shifted 1 and add a '0' bit for writing
     i2c_master_send(CTRL1_XL_ADDR); //SUB: send an 8-bit sub-address of CTRL1_XL_ADDR register used
     i2c_master_send(0b10000010); // DATA: send 8-bit data to set sample rate to 1.66 kHz, with 2g sensitivity, and 100 Hz filter
     i2c_master_stop(); //SP: stop bit
-    
+
     // Turn on gyroscope (Register CTRL2_G)
     i2c_master_start(); //ST: start bit
-    i2c_master_send(SLAVE_ADDR<<1|0); //SAD+W: slave address left shifted 1 and add a '0' bit for writing
+    i2c_master_send(SLAVE_ADDR << 1 | 0); //SAD+W: slave address left shifted 1 and add a '0' bit for writing
     i2c_master_send(CTRL2_G_ADDR); //SUB: send an 8-bit sub-address of CTRL2_G_ADDR register used
     i2c_master_send(0b10001000); // DATA: send 8-bit data to set sample rate to 1.66 kHz, 1000 dps
     i2c_master_stop(); //SP: stop bit
-    
+
     // Change Register CTRL3_C 
     i2c_master_start(); //ST: start bit
-    i2c_master_send(SLAVE_ADDR<<1|0); //SAD+W: slave address left shifted 1 and add a '0' bit for writing
+    i2c_master_send(SLAVE_ADDR << 1 | 0); //SAD+W: slave address left shifted 1 and add a '0' bit for writing
     i2c_master_send(CTRL3_C_ADDR); //SUB: send an 8-bit sub-address of CTRL3_C_ADDR register used
     i2c_master_send(0b00000100); // DATA: send 8-bit data to set everything default, ensuring IF_INC bit is '1'
     i2c_master_stop(); //SP: stop bit
@@ -140,19 +209,20 @@ void IMU_init(){
 
 void I2C_read_multiple(unsigned char address, unsigned char register1, unsigned char * data, int length) {
     i2c_master_start(); //ST: start bit
-    i2c_master_send(address<<1|0); //SAD+W: slave address left shifted 1 and add a '0' bit for writing
+    i2c_master_send(address << 1 | 0); //SAD+W: slave address left shifted 1 and add a '0' bit for writing
     i2c_master_send(register1); //SUB: send an 8-bit sub-address of register used
     i2c_master_restart(); // SR: make restart bit
-    i2c_master_send(address<<1|1); // SAD+R: slave address left shifted 1 and add a '1' bit for reading
+    i2c_master_send(address << 1 | 1); // SAD+R: slave address left shifted 1 and add a '1' bit for reading
     int i = 0;
-    for(i=0; i<length-1; i++) {
+    for (i = 0; i < length - 1; i++) {
         data[i] = i2c_master_recv(); //DATA: save 13 8-bit bytes 
         i2c_master_ack(0); // MAK
     }
-    data[length-1] = i2c_master_recv(); //DATA: save the 14th 8-bit bytes
+    data[length - 1] = i2c_master_recv(); //DATA: save the 14th 8-bit bytes
     i2c_master_ack(1); // NMAK
     i2c_master_stop(); //SP: stop bit
 }
+
 int main(void) {
     __builtin_disable_interrupts();
 
@@ -174,7 +244,7 @@ int main(void) {
     LATAbits.LATA4 = 1; //sets Green LED to be high output 
     ANSELBbits.ANSB2 = 0; //turn off analog function on pin 6 of pic32
     ANSELBbits.ANSB3 = 0; //turn off analog function on pin 7 of pic32
-    
+
     i2c_master_setup(); //turns on I2C peripheral
     IMU_init(); //initialize IMU
     SPI1_init(); //initialize SPI1
@@ -185,18 +255,18 @@ int main(void) {
     char msg[100];
     unsigned char charArray[100]; // to store 8 bit bytes
     signed short shortArray[100]; // to store 16 bit shorts after recombination from charArray
-   
-    
-    while(1) {
+
+
+    while (1) {
         //sprintf(msg, "Temp: %d", getWHOAMI());
         //display_string(msg,10,20);
-        
+
         I2C_read_multiple(SLAVE_ADDR, OUT_TEMP_L_ADDR, charArray, 14); //reads 14 8-bit bytes of data, into the charArray
         int j = 0;
-        for(j=0; j<7; j++){ //recombines charArray data into 7 16-bit shorts
-            shortArray[j] = ((charArray[2*j+1] << 8) | (charArray[2*j])); 
+        for (j = 0; j < 7; j++) { //recombines charArray data into 7 16-bit shorts
+            shortArray[j] = ((charArray[2 * j + 1] << 8) | (charArray[2 * j]));
         }
-        
+        /*
         sprintf(msg, "Temp: %hi", shortArray[0]);
         display_string(msg,10,20); 
         sprintf(msg, "gyroX: %hi", shortArray[1]);
@@ -205,17 +275,25 @@ int main(void) {
         display_string(msg,10,40); 
         sprintf(msg, "gyroZ: %hi", shortArray[3]);
         display_string(msg,10,50);
+         */
         sprintf(msg, "accelX: %hi", shortArray[4]);
-        display_string(msg,10,60);
+        display_string(msg, 10, 10);
         sprintf(msg, "accelY: %hi", shortArray[5]);
-        display_string(msg,10,70);
-        sprintf(msg, "accelZ: %hi", shortArray[6]);
-        display_string(msg,10,80); 
-        
-        while(_CP0_GET_COUNT() <= 4800000){
+        display_string(msg, 10, 20);
+        signed short scaled_accelX = shortArray[4] / 500;
+        signed short scaled_accelY = shortArray[5] / 500;
+
+        draw_bar_X(64, 63, 2, BARCOLOR, BG, scaled_accelX, MAXBARLEN);
+        draw_bar_Y(64, 63, 2, BARCOLOR, BG, scaled_accelY, MAXBARLEN);
+
+
+        //sprintf(msg, "accelZ: %hi", shortArray[6]);
+        //display_string(msg,10,80); 
+
+        while (_CP0_GET_COUNT() <= 4800000) {
             ; //do nothing for 0.2s to achieve 5Hz
         }
-         
+
     }
 
 }
